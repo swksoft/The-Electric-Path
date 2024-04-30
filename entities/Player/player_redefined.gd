b36e75875be9ac50d9f824df1304572dd50ec19c
swksoft <esparzapatricio95@gmail.com>
2024-04-30 11:16:53 -0300

    creo que funciona este XD

--- entities/Player/player_redefined.gd
+++ entities/Player/player_redefined.gd
@@ -3,15 +3,15 @@ extends RigidBody2D
 @export var radius: float = 10.0
 
 enum PlayerState {
-	INSIDE_LINE,
-	OUTSIDE_LINE
+	OUTSIDE_LINE,
+	INSIDE_LINE
 }
 var current_state := PlayerState.OUTSIDE_LINE
 
 var current_line = null
 var current_line_position = Vector2.ZERO
 var time_on_current_line := 0.0
-var max_time_on_current_line := 3.0
+var max_time_on_current_line := 2.0
 
 func _ready():
 	draw_circle_polygon(32, radius)
@@ -26,38 +26,47 @@ func draw_circle_polygon(points_nb: int, rad: float) -> void:
 	$Polygon2D.polygon = points
 
 func _physics_process(delta):
+	print(current_state)
 	match current_state:
 		PlayerState.INSIDE_LINE:
 			time_on_current_line += delta
+			attach()
+			# TIME OUT
+			if current_line == null:
+				PlayerState.OUTSIDE_LINE
+			
 			if time_on_current_line >= max_time_on_current_line:
-				print("Time Out")
+				current_line = null
+				current_line_position = Vector2.ZERO
+				time_on_current_line = 0
+				
+				current_state = PlayerState.OUTSIDE_LINE
 		PlayerState.OUTSIDE_LINE:
-			# SEARCH FOR LINES
-			print("pass")
-	
-	# QUITAR
-	
-	# If there's no lines
-	if current_line == null:
-		return
-	
-	# If player inputs a Line2D
-	if is_near_line(current_line_position):
-		attach()
-		time_on_current_line += delta
-		# TIME OUT
-		if time_on_current_line >= max_time_on_current_line:
-			current_line = null
-			current_line_position = Vector2.ZERO
-			time_on_current_line = 0
-	else:
-		# Out of line
-		current_line = null
-		current_line_position = Vector2.ZERO
-		time_on_current_line = 0
+			# If there's no lines
+			if current_line == null:
+				#current_state = PlayerState.OUTSIDE_LINE
+				return
+			# If player inputs a Line2D
+			if is_near_line(current_line_position):
+				current_state = PlayerState.INSIDE_LINE
+				
+				time_on_current_line += delta
+				# TIME OUT
+				if time_on_current_line >= max_time_on_current_line:
+					current_line = null
+					current_line_position = Vector2.ZERO
+					time_on_current_line = 0
+				else:
+					# Out of line
+					current_line = null
+					current_line_position = Vector2.ZERO
+					time_on_current_line = 0
 	
 func is_near_line(line_position):
 	return global_position.distance_to(line_position) < 20
 
 func attach():
 	print("detected")
+
+func _on_gameplay_redefined_new_line():
+	current_state = PlayerState.OUTSIDE_LINE
