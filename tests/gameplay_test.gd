bf239fd61e2f7e6b4febb9b7354718669d08dc01
swksoft <esparzapatricio95@gmail.com>
2024-04-29 23:34:00 -0300

    test map

--- tests/gameplay_test.gd
+++ tests/gameplay_test.gd
@@ -1,7 +1,24 @@
 extends Node2D
 
+signal game_over
+
+var level = 0
+var game_over_flag : = false
+
 @onready var gameplay_redefined = $GameplayRedefined
 @onready var player = $Player
 
 func _physics_process(delta):
-	pass
+	var screen_size = get_viewport().get_visible_rect().size
+	var player_position = player.global_position
+
+	var is_inside_screen = (
+		player_position.x >= 0 and
+		player_position.x <= screen_size.x and
+		player_position.y >= 0 and
+		player_position.y <= screen_size.y
+	)
+
+	if not is_inside_screen and !game_over_flag:
+		game_over_flag = true
+		emit_signal("game_over")
