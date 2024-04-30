a475ede7ac87833c50fdab30406dcd42a567c0c0
swksoft <esparzapatricio95@gmail.com>
2024-04-30 12:25:39 -0300

    XD

--- entities/Player/player_redefined.gd
+++ entities/Player/player_redefined.gd
@@ -1,6 +1,9 @@
 extends RigidBody2D
 
+const MAX_SPEED = 1000
+
 @export var radius: float = 10.0
+@export var current_cable_path: NodePath
 
 enum PlayerState {
 	OUTSIDE_LINE,
@@ -12,6 +15,10 @@ var current_line = null
 var current_line_position = Vector2.ZERO
 var time_on_current_line := 0.0
 var max_time_on_current_line := 2.0
+
+@onready var poly = $Polygon2D
+@onready var current_cable = get_node(current_cable_path)
+@onready var collision = $CollisionShape2D
 
 func _ready():
 	draw_circle_polygon(32, radius)
@@ -25,8 +32,13 @@ func draw_circle_polygon(points_nb: int, rad: float) -> void:
 	
 	$Polygon2D.polygon = points
 
+func _integrate_forces(state):
+	var len = min(MAX_SPEED, state.linear_velocity.length())
+	state.linear_velocity = state.linear_velocity.normalized() * len
+
 func _physics_process(delta):
-	print(current_state)
+	#print(linear_velocity)
+	#print(current_state)
 	match current_state:
 		PlayerState.INSIDE_LINE:
 			time_on_current_line += delta
@@ -41,10 +53,12 @@ func _physics_process(delta):
 				time_on_current_line = 0
 				
 				current_state = PlayerState.OUTSIDE_LINE
+			
+			#$Polygon2D.visible = false
+			
 		PlayerState.OUTSIDE_LINE:
 			# If there's no lines
 			if current_line == null:
-				#current_state = PlayerState.OUTSIDE_LINE
 				return
 			# If player inputs a Line2D
 			if is_near_line(current_line_position):
@@ -61,12 +75,20 @@ func _physics_process(delta):
 					current_line = null
 					current_line_position = Vector2.ZERO
 					time_on_current_line = 0
-	
+
+			$Polygon2D.visible = true
+
 func is_near_line(line_position):
 	return global_position.distance_to(line_position) < 20
 
 func attach():
-	print("detected")
+	var condition = poly.global_position != current_cable.current_line.points[0] or poly.global_position != current_cable.current_line.points[-1]
+	#print(condition)
+	if condition:
+		print(poly.global_position)
+		print(current_cable.current_line.points[-1])
+		poly.global_position = current_line_position
+		collision.global_position = current_line_position
 
 func _on_gameplay_redefined_new_line():
 	current_state = PlayerState.OUTSIDE_LINE
