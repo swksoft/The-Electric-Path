7c264de78e0115a36f6ac71f74a6bc98f1267a8b
swksoft <esparzapatricio95@gmail.com>
2024-05-02 09:57:06 -0300

    Player damping while in cable

--- entities/Player/player_definitive.gd
+++ entities/Player/player_definitive.gd
@@ -17,14 +17,28 @@ var current_speed : Vector2
 var current_line_position = Vector2.ZERO
 var time_on_current_line := 0.0
 var max_time_on_current_line := 2.0
+var reduction_factor := 0.75
 
-@onready var poly = Polygon2D
+# DEBUG
+var line_debug_lenght := 50
+@onready var line_visualization := $Line2DDebug
+var last_point := Vector2.ZERO
+
+@onready var poly = $Polygon2D
 @onready var current_cable = get_node(current_cable_path)
 
 func _ready():
 	new_rigid_body = RigidBody2D.new()
 	new_rigid_body.global_position = position
+	
+	new_rigid_body.linear_damp_mode = RigidBody2D.DAMP_MODE_COMBINE
+	#new_rigid_body.linear_damp = 100
+	
+	# DEBUG
+	new_rigid_body.linear_velocity = Vector2(500,0)
+	
 	get_parent().add_child.call_deferred(new_rigid_body)
+	
 	draw_circle_polygon(32, radius)
 	
 func draw_circle_polygon(points_nb: int, rad: float) -> void:
@@ -34,11 +48,25 @@ func draw_circle_polygon(points_nb: int, rad: float) -> void:
 		var point = deg_to_rad(i * 360.0 / points_nb - 90)
 		points.push_back(Vector2.ZERO + Vector2(cos(point), sin(point)) * rad)
 
+func debug_line():
+	#var velocity_direction = (current_line_position - global_position).normalized()
+	var velocity_direction = (new_rigid_body.linear_velocity).normalized()
+	
+	line_visualization.clear_points()
+	line_visualization.add_point(poly.position)
+	
+	line_visualization.global_position = self.position
+	#last_point = poly.position + velocity_direction * line_debug_lenght
+	last_point = poly.position + velocity_direction * line_debug_lenght
+	line_visualization.add_point(last_point)
+
 func _physics_process(delta):
-	#var velocity_direction = (closest_point - node_position).normalized()
+	
 	
 	match current_state:
 		PlayerState.OUTSIDE_LINE:
+			debug_line()
+			
 			# Check if RigidBody (follow)
 			if new_rigid_body != null:
 				global_position = new_rigid_body.position
@@ -48,11 +76,6 @@ func _physics_process(delta):
 				return
 			if is_near_line(current_line_position):
 				current_state = PlayerState.INSIDE_LINE
-			
-			#current_cable.rigid_body_position = new_rigid_body.global_position
-				
-			# Polygon affected by physics
-			#marker_player.global_position = rigid.global_position
 			
 		PlayerState.INSIDE_LINE:
 			# Save current speed
@@ -73,21 +96,6 @@ func _physics_process(delta):
 				else:
 					# SE SUELTA
 					release()
-					
-					
-					#current_cable.is_used = true
-					
-					#new_rigid_body = RigidBody2D.new()
-					#new_rigid_body.global_position = position
-					
-					#get_parent().add_child.call_deferred(new_rigid_body)
-					
-					#global_position = global_position
-					
-					
-					#current_state = PlayerState.OUTSIDE_LINE
-					
-					#time_on_current_line = 0
 				
 				# TODO: Add physics
 
@@ -100,27 +108,49 @@ func start_timer(delta):
 		PlayerState.OUTSIDE_LINE
 		
 	time_on_current_line += delta
+	
+	#new_rigid_body.linear_velocity *= speed_reduction_factor
+	if time_on_current_line >= 0.3:
+		## Calcula el factor de reducción de velocidad en función del tiempo acumulado
+		#var reduction_percentage = clamp(time_on_current_line / max_time_on_current_line, 0.0, 1.0)
+		#var current_reduction = (1.0 - reduction_percentage) * reduction_factor
+
+		new_rigid_body.linear_damp += 0.05
+		
+		#var reduction_percentage = delta / max_time_on_current_line
+		#var current_reduction = (1.0 - reduction_percentage) * reduction_factor
+		#new_rigid_body.linear_velocity *= current_reduction
 
 func time_out():
 	if time_on_current_line >= max_time_on_current_line:
+		
+		#current_speed = Vector2.ZERO
 		return true
 		time_out()
 
 func release():
 	time_on_current_line = 0
 	
-	# Delete previus RigidBody
-	# TODO: lo de arriba
-	
 	current_line = null
 	current_state = PlayerState.OUTSIDE_LINE
 	current_cable.is_used = true
+	
+	# Delete previus RigidBody
+	get_parent().get_child(3).queue_free()
+	# TODO: lo de arriba
 
 	create_rigid_body()
 
 func create_rigid_body():
 	new_rigid_body = RigidBody2D.new()
+	
 	new_rigid_body.global_position = position
+	#new_rigid_body.linear_velocity = current_speed/1.5
+	new_rigid_body.apply_force(current_speed*10)
+	#new_rigid_body.linear_velocity = current_cable.current_line.points[-1] # ESTABA BIEN
+	new_rigid_body.linear_velocity = current_speed
+	
+	
 	get_parent().add_child.call_deferred(new_rigid_body)
 	
 func _on_gameplay_redefined_new_line():
